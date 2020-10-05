3099d3f7bc17bacfe452c9b2088909aa3a07beab
davewinnyk <70180828+davewinnyk@users.noreply.github.com>
2020-10-05 18:00:40 -0500

    Testing Hello World Function

---
+++ calcFunctions/calc.js
@@ -0,0 +1,36 @@
+exports.handler = function(event, context, callback) {
+    callback(null, {
+    statusCode: 200,
+    body: "Hello, World"
+    });
+}
+
+
+
+
+
+/*
+STUFF FROM DOCS https://docs.netlify.com/functions/build-with-javascript/#format
+{
+    "path": "Path parameter",
+    "httpMethod": "Incoming request's method name"
+    "headers": {Incoming request headers}
+    "queryStringParameters": {query string parameters }
+    "body": "A JSON string of the request payload."
+    "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
+}
+
+The context parameter includes information about the context in which the serverless function was called, like certain Identity user information, for example.
+
+The callback works much like the same parameter in an AWS Lambda function. Your handler should use the callback to return either an error (as the first parameter) or a response object, such as:
+{
+    "isBase64Encoded": true|false,
+    "statusCode": httpStatusCode,
+    "headers": { "headerName": "headerValue", ... },
+    "multiValueHeaders": { "headerName": ["headerValue", "headerValue2", ...], ... },
+    "body": "..."
+}
+
+
+
+*/
