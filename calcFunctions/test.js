4d0b83b70730365826c5320f66bba16a46a89dba
davewinnyk <70180828+davewinnyk@users.noreply.github.com>
2020-10-06 22:10:08 -0500

    add html?

--- calcFunctions/test.js
+++ calcFunctions/test.js
@@ -79,7 +79,21 @@ if (gramprice <= 0)
 
      return {
          statusCode: 200,
-         body: `${poundphrase} ${quapphrase} ${ouncephrase} ${halfphrase} ${quapphrase} ${quarterphrase} ${eighthphrase} ${gramphrase}`
+         body: `<!DOCTYPE html>
+         <html lang=\"en\">
+         <head>
+             <meta charset=\"UTF-8\">
+             <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+             <title>Document</title>
+         </head>
+         <body>
+             <h1>${strainphrase}</h1>
+             <br>
+             <p>${poundphrase} ${quapphrase} ${ouncephrase} ${halfphrase} ${quapphrase} ${quarterphrase} ${eighthphrase} ${gramphrase}</p>
+         </body>
+         </html>`
+         
+         //body: `${poundphrase} ${quapphrase} ${ouncephrase} ${halfphrase} ${quapphrase} ${quarterphrase} ${eighthphrase} ${gramphrase}`
        };
  }
   };
